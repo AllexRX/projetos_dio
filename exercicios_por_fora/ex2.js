a13d4cbd038e58260eecc9775df334155d7e22f3
Alex <35071142+AllexRX@users.noreply.github.com>
2026-01-27 20:30:42 -0300

    criando exercicio para ver se o número  é positivo/negativo e par/impar

---
+++ exercicios_por_fora/ex2.js
@@ -0,0 +1,6 @@
+// 2 - Faça um algoritmo para receber um número qualquer e imprimir na tela se o número é par ou ímpar, positivo ou negativo.
+
+let numero = -50;
+numero % 2 === 0 ? console.log("O número é par") : console.log("O número é ímpar");
+numero > 0 ? console.log("O número é positivo") : console.log("O número é negativo");
+
