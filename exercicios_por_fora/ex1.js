e7619006dea65016a0fd3bc50344eda566d978f0
Alex <35071142+AllexRX@users.noreply.github.com>
2026-01-27 20:26:02 -0300

    criando exercicio onde se lê 3 valores e compara se a soma dos dois primeiros é maior que o terceiro

---
+++ exercicios_por_fora/ex1.js
@@ -0,0 +1,12 @@
+// 1 - Faça um algoritmo que leia os valores de A, B, C e em seguida imprima na tela a soma entre A e B é mostre se a soma é menor que C.
+
+let a, b, c;
+a = 40;
+b = 20;
+c = 50;
+console.log(`a soma entra ${a} e ${b} é igual a ${a + b}`)
+if ((a + b) < c) {
+   console.log(`a soma é menor que ${c}`);
+} else {
+   console.log(`a soma não é menor que ${c}`);
+}
