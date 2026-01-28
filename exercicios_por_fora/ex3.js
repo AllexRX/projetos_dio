131d54353a618a2e31274ea44c96b5e43066c3da
Alex <35071142+AllexRX@users.noreply.github.com>
2026-01-27 21:23:51 -0300

    criação do exercicio 3

---
+++ exercicios_por_fora/ex3.js
@@ -0,0 +1,10 @@
+let num1, num2;
+num1 = 220;
+num2 = 10;
+if ( num1 === num2){
+   let num3 = num1 + num2;
+   console.log(`os números são iguais e a soma é ${num3}`)
+} else {
+   let num3 = num1 * num2;
+   console.log(`os números são diferentes e o produto é ${num3}`)
+}
